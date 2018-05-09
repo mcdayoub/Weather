8c43fd48d6bc1df2328297ad241023b864f9818e
mcdayoub <38268139+mcdayoub@users.noreply.github.com>
2018-05-08 23:34:07 -0400

    Initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const API_KEY = "13edeb459b08ea84cfb9f44b4857f4cc";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
